35bec6fde22d106e79afe5143872b8c3d4790be0
Vizia128 <julianzia@gmail.com>
2018-05-17 13:11:10 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include  "msp430g2533.h"
+#include  "msp430g2553.h"
 #include "math.h"
 #include "invsqrt4.h"
 
 #define PERIOD 800
 #define HALF_DUTY 400
-#define ACCURACY 128
+#define ACCURACY 256
 
 int adc[1] = {12};
 
@@ -16,40 +16,77 @@ int zeroOffset = 0;
 int maxOffset = 0;
 int timeToStart = 0;
 
-float test[2] = {0};
+float test[3][3] = {0};
+float test1[3] = {0};
+float test2[3] = {0};
+
+int count = 0;
 
 void adc_Setup();
 void ADC_Sample();
 void BiCalibration();
 void PWM_Setup();
 
+int i = 0;
 
 int main(){
     WDTCTL = WDTPW + WDTHOLD;
+    DCOCTL = 0;                     // Select lowest DCOx and MODx
     BCSCTL1 = CALBC1_16MHZ;          // Set range
     DCOCTL = CALDCO_16MHZ;           // Set DCO step + modulation
 
-    //adc_Setup();
-    //BiCalibration();
+    adc_Setup();
+    BiCalibration();
     PWM_Setup();
 
+    TA0CCR1 = 600;
+/*
+    for(i = 0; i < 120; i++){
+        if(i%3 == 0){
+            TA0CCR1 = 100;
+        }
+        else if(i%3 == 1){
+            TA0CCR1 = 400;
+        }
+        else if(i%3 == 2){
+            TA0CCR1 = 700;
+        }
+
+        while(0 == 0);
+        timeToStart = 0;
+        ADC_Sample();
+        if(TA0CCR1 == 100){
+            test[0][0] += adc[0];
+            test[1][0] += adc[0] + TA0CCR1 * iPos_to_iFieldNum / 800;
+            test[2][0] += adc[0] - TA0CCR1 * iPos_to_iFieldNum / 800;
+        }
+        else if(TA0CCR1 == 400){
+            test[0][1] += adc[0];
+            test[1][1] += adc[0] + TA0CCR1 * iPos_to_iFieldNum / 800;
+            test[2][1] += adc[0] - TA0CCR1 * iPos_to_iFieldNum / 800;
+        }
+        else if(TA0CCR1 == 700){
+            test[0][2] += adc[0];
+            test[1][2] += adc[0] + TA0CCR1 * iPos_to_iFieldNum / 800;
+            test[2][2] += adc[0] - TA0CCR1 * iPos_to_iFieldNum / 800;
+        }
+//        test[i%2] += adc[0];
+//        test[i%2 + 4] += adc[0] + TA0CCR1 * iPos_to_iFieldNum / 800;
+
+ */
+    //}
     for(;;){
-        TA0CCR1 = 200;
-
-        ADC_Sample();
-        test[0] = adc[0] - TA0CCR1 * iPos_to_iFieldNum / 800;
-        TA0CCR1 = 600;
-
-        ADC_Sample();
-        test[1] = adc[0] - iPos_to_iFieldNum * TA0CCR1 / 800;
+        int pb = 12;
+        pb += 1;
+        pb--;
     }
 }
 
 #pragma vector=TIMER1_A0_VECTOR     // Timer1 A0 interrupt service routine
 __interrupt void Timer1_A0 (void) {
+
     timeToStart = 1;
-    //__bic_SR_register_on_exit(CPUOFF);        // Clear CPUOFF bit from 0(SR)
-    //TA0CCR1 = adc[0] /2;
+    count++;
 }
 
 // ADC10 interrupt service routine
@@ -59,7 +96,7 @@ __interrupt void ADC10_ISR(void) {
 }
 
 void PWM_Setup(){
-    DCOCTL = 0;                     // Select lowest DCOx and MODx
+    //DCOCTL = 0;                     // Select lowest DCOx and MODx
     //BCSCTL1 = CALBC1_16MHZ;          // Set range
     //DCOCTL = CALDCO_16MHZ;           // Set DCO step + modulation
 
@@ -74,9 +111,9 @@ void PWM_Setup(){
     TA0CTL |= TASSEL_2 + MC_1;      // SMCLK, Up Mode (Counts to TA0CCR0)
 
         /*** Timer1_A Set-Up ***/
-    TA1CCR0 |= 1600;                    // Counter value
+    TA1CCR0 |= 16000000;                    // Counter value
     TA1CCTL0 |= CCIE;               // Enable Timer1_A interrupts
-    TA1CTL |= TASSEL_2 + MC_1;      // SMCLK, Up Mode (Counts to TA1CCR0)
+    TA1CTL |= TASSEL_2 + MC_1 + ID_0;      // SMCLK, Up Mode (Counts to TA1CCR0)
 
     __enable_interrupt();
 
@@ -106,7 +143,7 @@ void BiCalibration(){
 
     __delay_cycles(16000000);
 
-    unsigned long B_Field[2] = {0,0};
+    double B_Field[2] = {0,0};
     int smallest = 1024;
     int largest = 0;
     int noise = 0;
@@ -135,4 +172,3 @@ void BiCalibration(){
     iPos_to_iField = (B_Field[1] - B_Field[0]) / ACCURACY / PERIOD;
 
 }
-
